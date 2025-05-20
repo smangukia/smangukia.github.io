24e714ec6ed451a7135e036e2c535384b6972e20
smangukia <smangukia111@gmail.com>
2025-05-20 00:30:44 -0300

    Fix Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-// This file ensures compatibility with GitHub Pages
-const fs = require('fs');
-const path = require('path');
-
-// Import the TypeScript config
-require('ts-node/register');
-const tsConfig = require('./next.config.ts').default;
-
-// Add a custom export handler
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...tsConfig,
-  // Add this to ensure .nojekyll file is created
-  async afterExport() {
-    const outDir = path.join(process.cwd(), 'out');
-    fs.writeFileSync(path.join(outDir, '.nojekyll'), '');
-  }
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: false,
 };
 
 module.exports = nextConfig;
